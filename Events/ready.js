18a27e057caf1a74e084df044a4dd8d16088adb5
turnbass <pro.turnbass@gmail.com>
2024-02-18 01:52:24 +0100

    v 0.9
     Ajoue du status discord

--- Events/ready.js
+++ Events/ready.js
@@ -1,13 +1,19 @@
 const Discord = require('discord.js');
 const LoadSlashCommand = require('../Handler/LoadSlashCommand')
+const version = require('../package-lock.json')
 
 module.exports = async bot => {
+    try {
+        await LoadSlashCommand(bot);
+        
+        const activityMessage = `Gére les tickets | ${version.version}`;
+        await bot.user.setActivity(activityMessage, { type: Discord.ActivityType.Watching });
 
-    await LoadSlashCommand(bot)
-
-
-    console.log('================================')
-    console.log(`${bot.user.tag} Is online`)
-    console.log("Ready!") // Do not touch this is for the panel to tell is online
-    console.log('================================')
+        console.log('================================');
+        console.log(`${bot.user.tag} Is online`);
+        console.log('Ready!'); // Assurez-vous que la chaîne de caractères est correcte
+        console.log('================================');
+    } catch (error) {
+        console.error('Error during bot initialization:', error);
+    }
 }
